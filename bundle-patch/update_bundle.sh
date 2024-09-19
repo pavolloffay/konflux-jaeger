785e1602646cd5e045af8db9aa152fae9976abcc
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-19 16:53:29 +0000

    Update jaeger-es-rollover to f9800ac

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -13,7 +13,7 @@ export JAEGER_QUERY_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/
 # Separate due to merge conflicts
 export JAEGER_ALL_IN_ONE_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-all-in-one@sha256:4adbb6a142382d1706b3dc020a9c84eae6ccf343160d8f8ecb3004a728e58c40"
 # Separate due to merge conflicts
-export JAEGER_ROLLOVER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-es-rollover@sha256:b642411994a80baa2f3161fa190ba04a5ac0a38dcee76275d7fc06a071d3156e"
+export JAEGER_ROLLOVER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-es-rollover@sha256:f9800ac9909abaef4dc1a28a1a7400ef78b775fa3427498704ac8431c579fee8"
 # Separate due to merge conflicts
 export JAEGER_INDEX_CLEANER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-es-index-cleaner@sha256:973f22e2da8532d7a56ea20f037dec4141f273a2f787ccf4bae49dd8c007af4d"
 # Separate due to merge conflicts
