8dc6f07c3af93f53c4a4134032f96663a04c4248
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-30 09:13:36 +0000

    Update jaeger-es-index-cleaner to 743e8cd
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -15,7 +15,7 @@ export JAEGER_ALL_IN_ONE_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-te
 # Separate due to merge conflicts
 export JAEGER_ROLLOVER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-es-rollover@sha256:fa4920456a43f06cafd07de69ed7b9a5c3e4b7b1f419d45bc3bf327c976fd01b"
 # Separate due to merge conflicts
-export JAEGER_INDEX_CLEANER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-es-index-cleaner@sha256:2841013ea5a9d718905ac7c34fddcac7057cf33579a616033052af70a6413d29"
+export JAEGER_INDEX_CLEANER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-es-index-cleaner@sha256:743e8cdd886a6b2c8d6ce1bf1fad667cf069540ede08f80f243b8dbc7ff5d787"
 # Separate due to merge conflicts
 export JAEGER_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-operator@sha256:283c03809769a7885e4ec5356b7c72b387444d77aa2aed7a6cd8d799dbce8204"
 # Separate due to merge conflicts
